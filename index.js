fd5f2465fce56b87e7b58adff1c1f1b3389b678a
Nikolas Furtado <nikolasfurtado@gmail.com>
2022-07-28 22:06:51 -0400

    buttons from keyboard working properly

--- index.js
+++ index.js
@@ -1,17 +1,62 @@
 var drum = document.querySelectorAll(".drum");
 
+let keyPressed;
+addEventListener("keydown", (e) => {
+    keyPressed = (e.key);
+
+    switch (keyPressed) {
+        case 'w':
+            let sd1 = new Audio('sounds/tom-1.mp3');
+            sd1.play();
+            break;
+        case 'a':
+            let sd2 = new Audio('sounds/tom-2.mp3');
+            sd2.play();
+            break;
+        case 's':
+            let sd3 = new Audio('sounds/tom-3.mp3');
+            sd3.play();
+            break;
+        case 'd':
+            let sd4 = new Audio('sounds/tom-4.mp3');
+            sd4.play();
+            break;
+        case 'j':
+            let sd5 = new Audio('sounds/tom-5.mp3');
+            sd5.play();
+            break;
+        case 'k':
+            let sd6 = new Audio('sounds/tom-6.mp3');
+            sd6.play();
+            break;
+        case 'l':
+            let sd7 = new Audio('sounds/tom-7.mp3');
+            sd7.play();
+            break;
+
+        default:
+            break;
+    }
+
+});
 for (let i = 0; i < drum.length; i++) {
 
-    addEventListener("keydown", () => {
-        alert("a key was pressed");
-    });
+    var currentI = i;
 
     drum[i].addEventListener("click", () => {
 
-        let sd = new Audio('sounds/tom-' + (i + 1) + '.mp3');
+        var sd = new Audio('sounds/tom-' + (i + 1) + '.mp3');
         sd.play();
 
-        drum[i].setAttribute("style", "color:white");
+        drum[i].toggleAttribute("style", "color:white");
+
+
+
 
     });
+
+
+
+
+
 }
